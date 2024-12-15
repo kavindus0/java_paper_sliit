352c5c50c87030d061a0eee0cdb2c5fe13f8f525
kavindus0 <kavindusachinthe@outlook.com>
2024-12-15 18:45:42 +0530

    factorial calling tested

--- src/factorial_using_threads/MainThreadApp.java
+++ src/factorial_using_threads/MainThreadApp.java
@@ -4,5 +4,6 @@ public class MainThreadApp {
     public static void main(String[] args) {
         Calculation calculation = new Calculation();
         calculation.factorial(5,10);
+        System.out.println(calculation.getAns());
     }
 }
