bb66d8f2eec1c42398c45336894aa48f8476cd66
kavindus0 <kavindusachinthe@outlook.com>
2024-12-15 16:55:56 +0530

    extends thread and override run method

--- src/thread_class_if/CountDown.java
+++ src/thread_class_if/CountDown.java
@@ -2,4 +2,22 @@ package thread_class_if;
 
 public class CountDown extends Thread{
 
-}
+@Override
+    public void run() {
+    synchronized (CountDown.class) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+1);
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
+    }
+
+
+        }
+    }
+
