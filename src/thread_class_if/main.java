3d9e03a1e30be3778801695accf7351b614947bb
kavindus0 <kavindusachinthe@outlook.com>
2024-12-15 17:02:59 +0530

    commented previous code with Thread Class

--- src/thread_class_if/main.java
+++ src/thread_class_if/main.java
@@ -4,7 +4,12 @@ public class main
 {
 
     public static void main(String[] args) {
-        CountDown ncow = new CountDown();
-        ncow.start();
+//        CountDown ncow = new CountDown();
+//        ncow.start();
+//
+
+            ClacSum nwclac = new ClacSum();
+            nwclac.run();
+
     }
 }
