f95c0e42b46063d843a3cf8cb95d338086de8503
kavindus0 <kavindusachinthe@outlook.com>
2024-12-15 18:37:19 +0530

    factorial method implemented

--- src/factorial_using_threads/Calculation.java
+++ src/factorial_using_threads/Calculation.java
@@ -6,9 +6,14 @@ public class Calculation {
         return ans;
     }
 
-    private double ans;
+    private double ans = 1;
 
     void factorial(int start, int end) {
+        while (start <= end) {
+            ans = ans * start;
+            start++;
+        }
+        System.out.println(ans);
 
     }
 
