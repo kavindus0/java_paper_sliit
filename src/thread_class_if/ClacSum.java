9070dada7aa90187b77558cc75f79ece8def72ea
kavindus0 <kavindusachinthe@outlook.com>
2024-12-15 17:01:28 +0530

    Runnable interface implemented and calculated function created

---
+++ src/thread_class_if/ClacSum.java
@@ -0,0 +1,16 @@
+package thread_class_if;
+
+public class ClacSum implements Runnable {
+    long sum = 0;
+    @Override
+    public void run() {
+        synchronized (ClacSum.class) {
+            for (int i = 1; i <= 10000; i++) {
+                sum += i;
+            }
+            System.out.println(sum);
+
+        }
+
+    }
+}
