6086879f8d361f317450d72683bac57f0a9608b6
kavindus0 <kavindusachinthe@outlook.com>
2024-12-15 18:45:20 +0530

    code moved

--- src/factorial_using_threads/Calculation.java
+++ src/factorial_using_threads/Calculation.java
@@ -2,6 +2,16 @@ package factorial_using_threads;
 
 public class Calculation {
 
+
+    private double ans=1;
+
+    void factorial(int start, int end) {
+        while (start <= end) {
+            ans = ans * (double) start;
+            start++;
+        }
+
+    }
     public double getAns() {
         return ans;
     }
