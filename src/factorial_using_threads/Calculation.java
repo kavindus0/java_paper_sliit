15fe8fb548cec94ab45b69f76c8f2bc67ee908ac
kavindus0 <kavindusachinthe@outlook.com>
2024-12-15 18:45:27 +0530

    code moved

--- src/factorial_using_threads/Calculation.java
+++ src/factorial_using_threads/Calculation.java
@@ -16,15 +16,4 @@ public class Calculation {
         return ans;
     }
 
-    private double ans = 1;
-
-    void factorial(int start, int end) {
-        while (start <= end) {
-            ans = ans * start;
-            start++;
-        }
-        System.out.println(ans);
-
-    }
-
 }
