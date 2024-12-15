d605f89108a5623931c1d948e463b4e0662441bf
kavindus0 <kavindusachinthe@outlook.com>
2024-12-15 15:55:21 +0530

    Item Super class

---
+++ src/Item.java
@@ -0,0 +1,15 @@
+public class Item {
+    int itemNo;
+    String description;
+    int unitPrice;
+
+    public Item() {
+    }
+    // Constructor Created
+
+    public void Display() {
+        System.out.println(itemNo);
+        System.out.println(description);
+        System.out.println(unitPrice);
+    }
+}
