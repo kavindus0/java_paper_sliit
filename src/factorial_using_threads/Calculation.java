edb851cd862e3615671d02c5c3e0fa6032cdf78c
kavindus0 <kavindusachinthe@outlook.com>
2024-12-15 17:41:16 +0530

    new facctorial calculated using thread class

---
+++ src/factorial_using_threads/Calculation.java
@@ -0,0 +1,15 @@
+package factorial_using_threads;
+
+public class Calculation {
+
+    public double getAns() {
+        return ans;
+    }
+
+    private double ans;
+
+    void factorial(int start, int end) {
+
+    }
+
+}
