c0a3dd723b2c2a6c76a778226a52a0fc16a31689
kavindus0 <kavindusachinthe@outlook.com>
2024-12-15 17:09:37 +0530

    new countdown obj created

--- src/thread_class_if/MainThreadApp.java
+++ src/thread_class_if/MainThreadApp.java
@@ -4,7 +4,8 @@ public class MainThreadApp
 {
 
     public static void main(String[] args) {
-//        CountDown ncow = new CountDown();
+
+        CountDown threadObj = new CountDown();
 //        ncow.start();
 //
         CalcSum nwclac = new CalcSum();
