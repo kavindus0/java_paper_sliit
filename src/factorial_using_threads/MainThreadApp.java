87a231b0f2d448f6bcf15092d587140a98e244ce
kavindus0 <kavindusachinthe@outlook.com>
2024-12-15 18:37:38 +0530

    MainThreadApp.java created

---
+++ src/factorial_using_threads/MainThreadApp.java
@@ -0,0 +1,8 @@
+package factorial_using_threads;
+
+public class MainThreadApp {
+    public static void main(String[] args) {
+        Calculation calculation = new Calculation();
+        calculation.factorial(5,10);
+    }
+}
